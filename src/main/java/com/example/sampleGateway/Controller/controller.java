61d74af76c12efc71c105454418bd5cdf9b6cb3d
Raphyan bala <1951016@cit.edu.in>
2022-06-24 07:56:58 +0530

    first commit

---
+++ src/main/java/com/example/sampleGateway/Controller/controller.java
@@ -0,0 +1,15 @@
+package com.example.sampleGateway.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+	
+	@GetMapping("/hello")
+	public String getHello()
+	{
+		return "hello";
+	}
+
+}
---
+++ src/test/java/com/example/sampleGateway/SampleGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sampleGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
